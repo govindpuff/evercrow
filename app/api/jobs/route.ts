1a3d64703ead850c6d5626e391d0668440e58d95
govindpuff <govindbrahmanyapura@gmail.com>
2024-06-15 16:49:53 -0700

    try again lol

--- app/api/jobs/route.ts
+++ app/api/jobs/route.ts
@@ -1,6 +1,10 @@
 import { sql } from "@vercel/postgres"
 import { NextResponse } from "next/server"
 
+export const dynamic = "force-dynamic"
+export const fetchCache = "force-no-store"
+export const revalidate = 0
+
 export async function GET(request: Request) {
   const { rows } =
     await sql<ProcessBirdsResultRow>`SELECT * FROM process_birds_results;`
